4e3026fd59f3eee692701c79c02b778d67f29e1c
Angelique Husson <44775743+AngeliqueHusson@users.noreply.github.com>
2020-05-30 21:50:04 +1000

    Add files via upload

---
+++ build/precache-manifest.bd1cc28a92e57f5361ab6ca4b9fcbd2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bebae5190d934cc81cab84594e631275",
+    "url": "/MathClassification/index.html"
+  },
+  {
+    "revision": "36fe9cc3ef275b0aabe9",
+    "url": "/MathClassification/static/css/main.5130dcd6.chunk.css"
+  },
+  {
+    "revision": "fd9d623d1608e5ba06dc",
+    "url": "/MathClassification/static/js/2.84ce2ed0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MathClassification/static/js/2.84ce2ed0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36fe9cc3ef275b0aabe9",
+    "url": "/MathClassification/static/js/main.654517d6.chunk.js"
+  },
+  {
+    "revision": "c34d7b2a3fc0e9477c4b",
+    "url": "/MathClassification/static/js/runtime-main.5f4f5fd5.js"
+  }
+]);
